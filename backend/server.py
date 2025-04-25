8987164f7b544f6d6b900fcd191cc635648aff20
Jeremy Yong <jeremy.yong@tetramem.com>
2025-04-25 16:46:13 -0700

    Add socket

--- backend/server.py
+++ backend/server.py
@@ -1,22 +1,20 @@
-from flask import Flask, request
-import requests
+from flask import Flask
+from flask_socketio import SocketIO, emit
 
 server = Flask(__name__)
+socketio = SocketIO(server)
 
 @server.route('/')
 def home():
     return "Hello, Flask!"
 
-@server.route('/user_context', methods=['POST'])
-def parse_user_context():
-    """
-    An endpoint to receive user context data.
-    
-    Expects a JSON body with a key 'user_context' containing the user input.
-    """
-    data = request.json
-    res = None # Generate response
-    return res
+@socketio.on('connect')
+def emit_data():
+    data = {'message': 'Hello, World!'}
+    while True:
+        emit('message', data)
+        print("Emitting data")
+        socketio.sleep(5)
 
 if __name__ == '__main__':
     server.run(debug=True)
