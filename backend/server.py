8063180f9a30776a65f09deb31a35eaa256be8a0
Jeremy Yong <jeremy.yong@tetramem.com>
2025-04-25 13:47:48 -0700

    Basic endpoint

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request
+import requests
 
 server = Flask(__name__)
 
 @server.route('/')
 def home():
     return "Hello, Flask!"
+
+@server.route('/user_context', methods=['POST'])
+def parse_user_context():
+    """
+    An endpoint to receive user context data.
+    
+    Expects a JSON body with a key 'user_context' containing the user input.
+    """
+    data = request.json
+    res = None # Generate response
+    return res
 
 if __name__ == '__main__':
     server.run(debug=True)
