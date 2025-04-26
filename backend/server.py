97b41863e15520acd764c7e62998f42033fdf998
Jeremy Yong <jeremy.yong@tetramem.com>
2025-04-25 17:18:31 -0700

    Add basic endpoints

--- backend/server.py
+++ backend/server.py
@@ -4,17 +4,22 @@ from flask_socketio import SocketIO, emit
 server = Flask(__name__)
 socketio = SocketIO(server)
 
-@server.route('/')
-def home():
-    return "Hello, Flask!"
+@socketio.on("signin")
+def sign_in(data):
+    pass
 
-@socketio.on('connect')
-def emit_data():
-    data = {'message': 'Hello, World!'}
-    while True:
-        emit('message', data)
-        print("Emitting data")
-        socketio.sleep(5)
+@socketio.on("signup")
+def sign_up(data):
+    pass
+
+@socketio.on("update_context")
+def update_context(data):
+    pass
+
+@socketio.on("submit_feedback")
+def submit_feedback(data):
+    pass
+
 
 if __name__ == '__main__':
     server.run(debug=True)
